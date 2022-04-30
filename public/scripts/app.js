1531f6ab2b5767b8725161a217dddf7349ad4e3a
mo4rahman <mo4rahman@gmail.com>
2022-04-30 15:59:34 -0400

    changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,6 @@ const allLikeBtns = document.querySelectorAll(".btn.btn-dark");
 console.log(allLikeBtns);
 console.log("SCRIPT IS RUNNING");
 
-
 for (let eachLikeBtn of allLikeBtns) {
   eachLikeBtn.addEventListener("click", function (event) {
     // event.preventDefault();
@@ -15,7 +14,5 @@ for (let eachLikeBtn of allLikeBtns) {
     console.log(
       document.querySelector(`#like-display${eachLikeBtn.id.slice(-1)}`).value
     );
-    // eachLikeBtn.submit();
   });
 }
-
